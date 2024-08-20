e93e490052acca96d4c6b26ee682680dfe13e401
Md Azharuddin <mdazharuddin155332@gmail.com>
2024-08-20 10:22:10 +0530

    Add Home component and Login component

--- react_router/src/App.jsx
+++ react_router/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
+import Home from './Components/Home'
 
 function App() {
 
   return (
     <>
      <Header/>
+    <Home/>
      <Footer/>
     </>
   )
